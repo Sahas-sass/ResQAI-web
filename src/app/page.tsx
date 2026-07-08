06b615d68eacc465750f2d2d70181a056e38d7e0
Sahas-sass <sahas.samuditha@gmail.com>
2026-07-09 00:47:21 +0530

    add ServiceCards and AboutSection components to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Hero from "@/components/home/Hero";
+import ServiceCards from "@/components/home/ServiceCards";
+import AboutSection from "@/components/home/AboutSection";
 
 export default function Home() {
   return (
     <div className="w-full flex flex-col min-h-screen">
       <Hero />
+      <ServiceCards />
+      <AboutSection />
     </div>
   );
 }
